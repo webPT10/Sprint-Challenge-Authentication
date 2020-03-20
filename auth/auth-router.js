7fac5477cd0df8d590e161e8c6f23c0c4a76d6fc
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-20 16:31:19 -0700

    npm i cookie-parser + setup basic structure in auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const Users = require("../users/usersModel")
 
+const jwt = require("jsonwebtoken")
+
+// POST	/register
+// Creates a user using the information sent inside the body of the request.
+// Hash the password before saving the user to the database.
+//-------------------
 router.post('/register', (req, res) => {
   // implement registration
 });
 
+// POST	/api/login
+// Use the credentials sent inside the body to authenticate the user.
+// On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id.
+// If login fails, respond with the correct status code and the message: 'You shall not pass!'
+//-------------------
 router.post('/login', (req, res) => {
   // implement login
 });
